3e40cb1e184a2e8b03536036cd8caf625e0ce572
balasm <01142551@pw.edu.pl>
2020-10-13 23:06:04 +0200

    added navbar /w comments

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React from 'react';
 import './App.css';
 import Router from "./router/Router";
 import {BrowserRouter} from "react-router-dom";
+import {MyNavbar} from "./components";
 
 function App() {
   return (
     <BrowserRouter>
       <div className="App">
+        <MyNavbar />
         <Router />
       </div>
     </BrowserRouter>
--- src/router/Router.jsx
+++ src/router/Router.jsx
@@ -2,10 +2,15 @@ import React from 'react';
 import routes from "../constants/routes"
 import {Route, Switch, useLocation} from "react-router";
 
-const Router = () => {
+const Router = (props) => {
+    // nazwa = (props) => {...}, argumentów używamy props.arg1, props.arg2 itd
+    // nazwa = ({arg1, arg2}) => {...}, argumentów używamy arg1, arg2 itd
+
+    // useLocation korzysta z window.location
     const location = useLocation();
 
     return (
+        // poniżej arrow function, zamiast tego można by napisać function() {...}, ale to czasami psuje odniesienie z this, więc ja np nie lubię xd
         () => {
             return (
                 <Switch location={location} key={location.pathname}>
@@ -13,7 +18,7 @@ const Router = () => {
                         ({component: Component, path, ...rest}) => {
                             return (
                                 <Route path={path} key={path} {...rest}>
-                                    <Component {...rest} />
+                                    <Component {...props} {...rest} />
                                 </Route>
                             );
                         }
