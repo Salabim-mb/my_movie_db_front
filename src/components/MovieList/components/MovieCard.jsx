66793c901d086340cdd3b6663ff4e4f8956be178
Krisu17 <01142550@pw.edu.pl>
2020-10-21 23:53:59 +0200

    added buttons

--- src/components/MovieList/components/MovieCard.jsx
+++ src/components/MovieList/components/MovieCard.jsx
@@ -4,18 +4,19 @@ import Button from "react-bootstrap/Button";
 import './MovieCard.css';
 import {paths} from "../../../constants/routes";
 import {IndexLinkContainer} from "react-router-bootstrap";
+import {backend} from "../../../constants/backend";
 
 const MovieCard = ({movie}) => (
     <div className="card mb-3">
         <div className="row no-gutters">
             <div className="ThumbnailImage">
-                <Card.Img src={movie.movieImage}/> {/*{movie.movieImage}*/}
+                <Card.Img src={movie.movieImage}/>
             </div>
             <div className="Button">
                 <IndexLinkContainer to={paths.NEW_MOVIE+"/"+movie.id}>
                     <Button variant="warning" size="sm">Edit</Button>
                 </IndexLinkContainer>
-                <div className="ButtonRemove">
+                <div className="ButtonRemove" onClick={handleRemoveMovie(movie.id)}>
                     <Button variant="danger" size="sm">Remove</Button>
                 </div>
             </div>
@@ -44,5 +45,33 @@ const MovieCard = ({movie}) => (
 
 );
 
+const handleRemoveMovie = async (id) => {
+    try {
+        await removeMovie(id);
+    } catch(e) {
+        console.log(e);
+    }
+}
+
+const removeMovie = async (id) => {
+    const headers = {
+        "Access-Control-Allow-Origin": "*",
+        "Content-Type": "application/json",
+    };
+
+    const url = backend.MOVIES + id + "/";
+
+    let res = await fetch(url , {
+        headers,
+        method: "DELETE"
+
+    });
+
+    if (res.status === 200) { // OK
+        return await res.json(); // return await oznacza, że body naszej odpowiedzi będzie zparsowane
+    } else {
+        throw res.status; // jeżeli status nie jest ok to go łapiemy w bloku try/catch metody loadMovieList()
+    }
+};
 
 export default MovieCard;
