78594b5cb1804fc7ca8d7f133453b34f4f4b6551
Krisu17 <01142550@pw.edu.pl>
2020-10-21 21:28:59 +0200

    Buttons added, and edit is working

--- src/components/MovieList/components/MovieCard.jsx
+++ src/components/MovieList/components/MovieCard.jsx
@@ -2,6 +2,8 @@ import React from "react";
 import Card from "react-bootstrap/Card";
 import Button from "react-bootstrap/Button";
 import './MovieCard.css';
+import {paths} from "../../../constants/routes";
+import {IndexLinkContainer} from "react-router-bootstrap";
 
 const MovieCard = ({movie}) => (
     <div className="card mb-3">
@@ -9,12 +11,22 @@ const MovieCard = ({movie}) => (
             <div className="ThumbnailImage">
                 <Card.Img src={movie.movieImage}/> {/*{movie.movieImage}*/}
             </div>
+            <div className="Button">
+                <IndexLinkContainer to={paths.NEW_MOVIE+"/"+movie.id}>
+                    <Button variant="warning" size="sm">Edit</Button>
+                </IndexLinkContainer>
+                <div className="ButtonRemove">
+                    <Button variant="danger" size="sm">Remove</Button>
+                </div>
+            </div>
+
             <div className="col-sm-9">
                 <div className="MovieCard">
                     <div className="card-body">
                         <Card.Title>
                             {movie.movieName}
                         </Card.Title>
+
                         <Card.Subtitle className="mb-2 text-muted">
                             {movie.movieDirector}
                         </Card.Subtitle>
@@ -31,5 +43,6 @@ const MovieCard = ({movie}) => (
     </div>
 
 );
+
 
 export default MovieCard;
